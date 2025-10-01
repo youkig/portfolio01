a6e612e85f4468a49cbf1d8925166e7c0b6d085f
youkig <youkiguitar@gmail.com>
2025-10-01 09:56:32 +0900

    Update config.php

--- seo_admin/config.php
+++ seo_admin/config.php
@@ -3,7 +3,7 @@
 //DB接続
 
 try{
-    $dbh = new PDO('mysql:host=mysql80.salmonyak7.sakura.ne.jp;dbname=salmonyak7_autometa','salmonyak7_autometa','Ja5pc9nTuMg');
+    $dbh = new PDO('mysql:host=hostname;dbname=dbname','userid','password');
 } catch (PDOException $e) {
      echo "接続失敗: ". $e->getMessage(). "\n";
     exit();
@@ -18,7 +18,7 @@ class LoginCh{
 
     public function logincheck($userid, $id){
         $sql = "SELECT * From user Where loginid=:email and id=:id and valid=1";
-        $dbh2 = new PDO('mysql:host=mysql80.salmonyak7.sakura.ne.jp;dbname=salmonyak7_autometa','salmonyak7_autometa','Ja5pc9nTuMg');
+        $dbh2 = new PDO('mysql:host=hostname;dbname=dbname','userid','password');
         $login = $dbh2->prepare($sql);
         $login->bindValue(":email", $userid, PDO::PARAM_STR);
         $login->bindValue(":id", $id, PDO::PARAM_STR);
@@ -27,9 +27,7 @@ class LoginCh{
         
         
         if($result = $login->fetch(PDO::FETCH_ASSOC)){
-            //return $this->username = $result['name'];
             $username = $result['name'];
-            
             return [$username];
         }else{
             header("location: login.php");
